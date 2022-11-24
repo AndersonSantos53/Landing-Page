eefce772e95006358fe54996871b9000a0a2442f
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-11-24 09:38:49 -0300

    Main

--- script.js
+++ script.js
@@ -1,4 +1,102 @@
-function Leitura(){
-    document.querySelector('#leitura').innerHTML = '<nav class="dis"> <img class="foto" src="https://thumbs2.imgbox.com/2a/a2/B7F18hTg_t.png"> </nav>'
+function handleClick(){
+    document.querySelector('#leitura').innerHTML =' <div class=" text-warning"> '+
+    'Sausage Man é um jogo battle royale de tiro competitivo em estilo cartoon estrelando salsichas como protagonistas. '+
+    'É um jogo que você consegue iniciar facilmente e jogar a qualquer hora e de qualquer lugar. Você assumirá o papel '+
+    'de salsichas fofas e engraçadas e travará batalhas dinâmicas e fantasiosas. '+
+  '</div> '+
+  '</div> '+
+  '<div class="row"> '+
+    '<div class="text-warning"> '+
+      '[Batalhas emocionantes e itens de fortalecimento com poderes únicos] '+
+      'Seja recebido com um sistema de batalha fluído e intenso, com trajetórias balísticas realistas e até um recurso '+
+      'de prender o '+
+      'fôlego no jogo. Enquanto isso, o jogo te fornece Sinalizadores, Máquinas de Ressurreição, Coberturas Táticas e '+
+      'sistemas de'+
+      'Cartão de Identificação que podem pôr à prova a camaradagem e a compreensão mútua entre você e seus '+
+      'companheiros. '+
+    '</div> '+
 
+    '<div class="row"> '+
+      '<div class="text-warning"> '+
+        '[Jogabilidade relaxante, solte sua imaginação e curta ficar à vontade] '+
+        'Há mais do que apenas combates no campo de batalha: você encontrará fofura e alegria por todo canto. Aqui você '+
+        'pode cantar, saltar '+
+        'e disparar suas armas numa Bola de Borracha ou usar um Salto Duplo para evitar disparos precisos dos inimigos. '+
+        'Você também pode colocar '+
+        'uma Bóia Salva-vidas e travar batalhas cara a cara na água com os outros. Quando você cair, você virará uma '+
+        'pequena salsicha birrenta. '+
+        'Você pode levantar os companheiros que sofreram queda com uma ação “Vamos lá”. '+
+      '</div> '+
+    '</div> '+
+    '<div class="row"> '+
+      '<div class=" text-warning"> '+
+        '[Visuais adoravelmente rudimentares, seja a estrela dessa festa alegre] '+
+        'O sistema de visual rudimentar, porém fofo do jogo te ajudará a tornar-se a salsicha mais popular que já '+
+        'existiu. O sistema único de '+
+        'Cartão de Grupo registra seus dados, aparências e conquistas, mostrando às outras salsichas o quão amável você'+
+        'é. Também te fornece '+
+        'vários conjuntos de fantasias peculiares como Koi, Cyberpunk e Empregada, e também poses ousadas fofas como '+
+        'assoprando beijos, transformações '+
+        'de garotas mágicas etc. Além disso, você também pode usar Emojis de Bolha como “Hastear Bandeira com Peça '+
+        'Íntima Branca” e “Choramingo '+
+        'de Injustiça” para interagir com outras salsichas. '+
+      '</div> '+
+    '</div> '+
+    '<div class="row"> '+
+      '<div class="text-warning"> '+
+        'Aqui você contará com sua “malandragem” e “fofura” para derrotar centenas de inimigos no campo de batalha e '+
+       'tornar-se o rei da festa! '+
+      '</div> '+
+    '</div> '
+}
+function handleClickReverse(){
+    document.querySelector('#leitura').innerHTML =' <div class="text-white"> '+
+    'Sausage Man é um jogo battle royale de tiro competitivo em estilo cartoon estrelando salsichas como protagonistas. '+
+    'É um jogo que você consegue iniciar facilmente e jogar a qualquer hora e de qualquer lugar. Você assumirá o papel '+
+    'de salsichas fofas e engraçadas e travará batalhas dinâmicas e fantasiosas. '+
+  '</div> '+
+  '</div> '+
+  '<div class="row"> '+
+    '<div class="text-white"> '+
+      '[Batalhas emocionantes e itens de fortalecimento com poderes únicos] '+
+      'Seja recebido com um sistema de batalha fluído e intenso, com trajetórias balísticas realistas e até um recurso '+
+      'de prender o '+
+      'fôlego no jogo. Enquanto isso, o jogo te fornece Sinalizadores, Máquinas de Ressurreição, Coberturas Táticas e '+
+      'sistemas de '+
+      'Cartão de Identificação que podem pôr à prova a camaradagem e a compreensão mútua entre você e seus '+
+      'companheiros. '+
+    '</div> '+
+
+    '<div class="row"> '+
+      '<div class="text-white"> '+
+        '[Jogabilidade relaxante, solte sua imaginação e curta ficar à vontade] '+
+        'Há mais do que apenas combates no campo de batalha: você encontrará fofura e alegria por todo canto. Aqui você '+
+        'pode cantar, saltar '+
+        'e disparar suas armas numa Bola de Borracha ou usar um Salto Duplo para evitar disparos precisos dos inimigos.'+
+        'Você também pode colocar '+
+        'uma Bóia Salva-vidas e travar batalhas cara a cara na água com os outros. Quando você cair, você virará uma '+
+        'pequena salsicha birrenta. '+
+        'Você pode levantar os companheiros que sofreram queda com uma ação “Vamos lá”. '+
+      '</div> '+
+    '</div> '+
+    '<div class="row"> '+
+      '<div class="text-white"> '+
+        '[Visuais adoravelmente rudimentares, seja a estrela dessa festa alegre] '+
+        'O sistema de visual rudimentar, porém fofo do jogo te ajudará a tornar-se a salsicha mais popular que já '+
+        'existiu. O sistema único de '+
+        'Cartão de Grupo registra seus dados, aparências e conquistas, mostrando às outras salsichas o quão amável você '+
+        'é. Também te fornece'+
+        'vários conjuntos de fantasias peculiares como Koi, Cyberpunk e Empregada, e também poses ousadas fofas como '+
+        'assoprando beijos, transformações '+
+        'de garotas mágicas etc. Além disso, você também pode usar Emojis de Bolha como “Hastear Bandeira com Peça '+
+        'Íntima Branca” e “Choramingo '+
+        'de Injustiça” para interagir com outras salsichas. '+
+      '</div> '+
+    '</div> '+
+    '<div class="row"> '+
+      '<div class="text-white"> '+
+        'Aqui você contará com sua “malandragem” e “fofura” para derrotar centenas de inimigos no campo de batalha e '+
+       'tornar-se o rei da festa! '+
+      '</div> '+
+    '</div> '
 }
